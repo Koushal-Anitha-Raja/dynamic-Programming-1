16f7c7eb80ff54bbc225e1b32c7251ff1df83460
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2022-10-10 00:37:35 -0400

    Add files via upload

---
+++ dynamicprogramming1/deleteandearn.py
@@ -0,0 +1,29 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        
+        #declaring a hashset for storing the nums value
+        Hset=[0] * (max(nums)+1)
+        
+        #iterating the nums array
+        for num in nums:
+            Hset[num]+=num
+            
+        #Folllowing the house robber strucutre
+        #declaring the dp array as the size of hashset we created
+        dp=[0] *len(Hset)
+        
+        #initializing the 0 th dp index as zero
+        dp[0]=0
+        
+        #initializing the 1 st index of dp array as 1 st index as 0 th index is zero
+        dp[1]=Hset[1]
+        
+        #iterating the value dp array and store max value
+        for i in range (2,len(dp)):
+            #logic to find the max value through one single pass
+            dp[i]=max((Hset[i]+dp[i-2],dp[i-1]))
+            
+        #returning the final element in the dp array as it stores the max value    
+            
+        return dp[-1]    
+        
---
+++ dynamicprogramming1/houserobber.py
@@ -0,0 +1,35 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """if len(nums)==1:
+            return nums[0]
+        
+        #using two variables
+        var1=nums[0]
+        var2=max(nums[0],nums[1])
+    
+        for i in range(2,len(nums)):
+            var2 = (max(nums[i] + var1, var2))
+        return var2"""
+        
+        #declaring a dp array 
+        dp=[]
+        
+        #if the input array has only one element
+        if len(nums)==1:
+            return nums[0]
+        #just append it directly to the dp array
+        dp.append(nums[0])
+        
+        #if the input array has two variable then check the max between them
+        dp.append(max(nums[0],nums[1]))
+    
+    
+        #iterate the input array nums except first two elements as we checked the conditions before itself
+        for i in range (2,len(nums)):
+            #the logic to carry the largest robbed value by using nums array and dp array
+            dp.append(max(nums[i]+dp[i-2],dp[i-1]))
+            
+        #returning the last element in dp array as it will carry the largest robbed value    
+        return dp[-1]    
+            
+            
